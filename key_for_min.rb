2a3f0af1270b571beba73044cf7ec9d6d01bd5f9
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-10-25 04:26:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 
 def key_for_min_value(name_hash)
   key = ""
+  min_value = 0
   name_hash.each do |name, price|
-    if price.min?
+    if min_value > price
+      min_value = price
       key = :name
-    else
-      key = nil
     end
   end
   key
