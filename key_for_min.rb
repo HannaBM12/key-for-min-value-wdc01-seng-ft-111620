9443ce0fa2860ef60acae3215e79ed3347c1bc90
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-10-25 04:21:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key = nil
+  key = ""
   name_hash.each do |name, price|
     if price.min
       key = :name
+    else
+      key = nil
     end
   end
   key
